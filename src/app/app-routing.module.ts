f0805ea590348573ff43e1aa0d1fee31804028b1
abradi1 <chaima.abradi@etu.unilim.fr>
2022-08-03 23:27:41 +0200

    final_check

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import {AcceuilComponent} from './components/acceuil/acceuil.component';
 
 const routes: Routes = [
+  {path: '', component:AcceuilComponent},
   { path: 'users', component: UsersListComponent },
+  
 ];
 
 @NgModule({
